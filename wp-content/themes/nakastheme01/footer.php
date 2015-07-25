be1f9a59cf30665f0f515eeb0fc4ec9428c3bc7f
User Nakas-me <nakas-me@www1608.sakura.ne.jp>
2015-07-25 17:30:33 +0900

    fix

--- wp-content/themes/nakastheme01/footer.php
+++ wp-content/themes/nakastheme01/footer.php
@@ -14,15 +14,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<?php
-				/**
-				 * Fires before the Twenty Fifteen footer text for footer customization.
-				 *
-				 * @since Twenty Fifteen 1.0
-				 */
-				do_action( 'twentyfifteen_credits' );
-			?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
+			<p id="copyright">Copyright © 2014-<?php echo date('Y'); ?> Yuta Nakagawa</p>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
 
--- wp-content/themes/nakastheme01/index.php
+++ wp-content/themes/nakastheme01/index.php
@@ -5,12 +5,6 @@
 
 		<?php if ( have_posts() ) : ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
 			<?php
 			// Start the loop.
 			while ( have_posts() ) : the_post();
